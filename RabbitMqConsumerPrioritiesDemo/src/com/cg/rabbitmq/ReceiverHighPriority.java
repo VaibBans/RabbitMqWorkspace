cb2ce9412c31267c49cbc1648d3fcfc5ea383593
navganes <navina.ganesan@capgemini.com>
2018-02-01 12:56:59 +0530

    new projects

---
+++ RabbitMqConsumerPrioritiesDemo/src/com/cg/rabbitmq/ReceiverHighPriority.java
@@ -0,0 +1,39 @@
+package com.cg.rabbitmq;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import com.rabbitmq.client.QueueingConsumer;
+
+
+public class ReceiverHighPriority {
+	private final static String PRIORITY_CONSUMER = "priorities_logs";
+	private final static String EXCHANGE_NAME = "exchange_logs";
+
+	public static void main(String[] args) throws Exception{
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(PRIORITY_CONSUMER, false, false, false, null);
+		channel.exchangeDeclare(EXCHANGE_NAME, "direct",true);
+		channel.queueBind(PRIORITY_CONSUMER,EXCHANGE_NAME,"");
+		System.out.println("Waiting for the message");
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		Map<String, Object> arguments = new HashMap<String, Object>();
+		arguments.put("x-priority", 10);
+		channel.basicConsume(PRIORITY_CONSUMER, false, "", false, false, arguments, consumer);
+
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println("Received '" + message + "'");
+			channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+		}
+	}
+}
---
+++ RabbitMqConsumerPrioritiesDemo/src/com/cg/rabbitmq/SenderPriorities.java
@@ -0,0 +1,29 @@
+package com.cg.rabbitmq;
+
+import java.util.Scanner;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class SenderPriorities {
+	private final static String PRIORITY_CONSUMER = "priorities_logs";
+	private final static String EXCHANGE_NAME = "exchange_logs";
+	
+	public static void main(String[] args) throws Exception{
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(PRIORITY_CONSUMER, false, false, false, null);
+		System.out.println("Enter your message");
+		Scanner sc = new Scanner(System.in);
+		String message = sc.nextLine();
+		channel.basicPublish("", PRIORITY_CONSUMER,null, message.getBytes("UTF-8"));		
+		System.out.println(" [x] Sent '" + message + "'");
+		
+		channel.close();
+		connection.close();
+		sc.close();
+	}
+
+}
